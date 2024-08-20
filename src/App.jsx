a6c83c7769abf333fd040d9d54d5e0f4f54ddf8b
Roberto_29 <robert292005@gmail.com>
2024-08-20 00:45:16 -0600

    Primer commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { useState, useEffect } from 'react';
 import { fetchClimaData } from './api';
 
@@ -15,11 +14,12 @@ function App() {
     getClimaData();
   }, []);
 
+
   return (
     <section className="min-h-screen bg-gradient-to-b from-blue-600 via-blue-500 to-blue-400 flex items-center justify-center gap-4">
       <div className="bg-white bg-opacity-20 p-8 rounded-2xl shadow-2xl backdrop-blur-md border-t border-b border-blue-200">
         <h1 className="text-2xl font-bold text-blue-900">Segundo Div</h1>
-        <p className="mt-4 text-blue-800">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+        <p className="mt-4 text-blue-800">Loremm ipsum dolor sit amet consectetur adipisicing elit.</p>
       </div>
       <div className="bg-white bg-opacity-20 p-8 rounded-2xl shadow-2xl backdrop-blur-md border-t border-b border-blue-200">
         <h1 className="text-7xl text-center font-bold text-blue-900">
@@ -29,24 +29,22 @@ function App() {
           <img className='max-w-full size-28 ' src="https://img.icons8.com/3d-fluency/94/storm.png" alt="Weather Icon" />
           <p className='text-2xl font-bold text-white '>{climaData !== null ? `${climaData.description.toUpperCase()}` : 'Obteniendo Datos...'}</p>
         </div>
-        <div>
-          <article>
-            <FontAwesomeIcon icon="fa-sharp fa-solid fa-droplet-percent" rotation={0} style={{color: "#1e4a94",}} />
+        <div className='flex justify-between'>
+          <article className='flex flex-col items-center justify-center' >
+            <img className='size-8' src="../public/svg/humedad.svg" alt="IMG-Humedad" />
+            <p> {climaData !== null ? `${climaData.humidity} %`: 'Obteniendo datos'} </p>
+            <p>Humedad</p>
+          </article>
+          <article className='flex flex-col items-center justify-center' >
+            <img className='size-8' src="../public/svg/viento.svg" alt="IMG-Viento" />
+            <p className="text-blue-800"> {climaData ? `${climaData.windSpeed} m/s` : 'Obteniendo Datos...'} </p>
+            <p className='text-center' >Velocidad <br /> de viento </p>
           </article>
           <article>
-
-          </article>
-          <article>
-
+            <p className="text-blue-800"> {climaData ? `${climaData.windSpeed} m/s` : 'Obteniendo Datos...'} </p>
+            <p>otra</p>
           </article>
         </div>
-
-        <p className="text-blue-800">
-          {climaData ? 
-           `Humedad: ${climaData.humidity}% \n
-            Velocidad del viento: ${climaData.windSpeed} m/s` : 
-           'Obteniendo Datos...'}
-        </p>
       </div>
     </section>
   );
--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-//30 74 148
+//#285091
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
